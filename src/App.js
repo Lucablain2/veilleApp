ee1980dd55dd3ce4cc2547f8c0199ef3b0fb6bfe
Lucas <b2ilucas@yahoo.fr>
2020-05-05 15:43:47 +0200

    maj APP.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  var x = "leo";
-  const Button = ({ label, style:{width}, onclick }) => {
-    return (
-      <button
-        onClick={onclick}
-        style={{
-          backgroundColor: 'lightgrey',
-          color: 'black',
-          borderRadius: 25,
-          padding: '10px 10px',
-          whiteSpace: 'nowrap',
-          width: '250px'
-        }}>
-        Chercher article
-      </button>
-    )
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.handleClick = this.handleClick.bind(this);
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Articles sur le REACT :
-        </p>
-        <Button
-          label="Recherhce article"
-          style={{width: 100}}
-          onClick={()=>console.log('clicked!')}
-        />
-      </header>
-      <body>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-        <div class="article"></div>
-      </body>
-    </div>
-  );
+
+  handleClick() {
+    alert('Fonction pour ajouter Article ICI !');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Articles sur le REACT :
+          </p>
+          <button onClick={this.handleClick}>Chercher article</button>
+        </header>
+        <body>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+          <div class="article"></div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
